1cd7d2138760cb76ca0b409f4cabde0ebb458c97
FOR-TIMI <timiplayskeys@gmail.com>
2022-10-31 06:19:45 -0400

    added populate friend

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -13,6 +13,10 @@ const userController = {
                             path: "thoughts",
                             select: "-__v",
                         })
+                        .populate({
+                            path: 'friends',
+                            select: '-__v',
+                        })
                         .select("-__v")
 
         if(!users.length){
@@ -36,6 +40,10 @@ const userController = {
                                 .populate({
                                     path: "thoughts",
                                     select: "-__v",
+                                })
+                                .populate({
+                                    path: 'friends',
+                                    select: '-__v',
                                 })
                                 .select("-__v")
         if(!user){
